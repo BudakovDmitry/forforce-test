2b6242b75ba39872d98c1298bf2595aec91dff77
Budakov Dmitry <budakov.it@gmail.com>
2022-11-05 15:02:37 +0200

    Add images

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,13 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[name][ext][query]",
+                },
+            },
         ],
     },
     devServer: {
