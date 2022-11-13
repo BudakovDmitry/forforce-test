5b074716d0a88a6ef36bf26e49432cebde6e8e09
Budakov Dmitry <budakov.it@gmail.com>
2022-11-13 16:09:48 +0200

    Update carousel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './styles.scss';
 import './index.html';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const carouselInner = document.querySelector('.carousel__inner');
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  let activeIndex = 0;
+
+  prevButton.addEventListener('click', () => {
+    activeIndex -= 1;
+    if (activeIndex < 0) {
+      activeIndex = carouselInner.children.length - 1;
+    }
+    carouselInner.style.transform = `translateX(-${activeIndex * 100}%)`;
+  });
+
+  nextButton.addEventListener('click', () => {
+    activeIndex += 1;
+    if (activeIndex > carouselInner.children.length - 1) {
+      activeIndex = 0;
+    }
+    carouselInner.style.transform = `translateX(-${activeIndex * 100}%)`;
+  });
+});
